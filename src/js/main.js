9048e1266407d5edb7c4fb36404c06043f63e28b
Jiaxin <jl2458@rutgers.edu>
2021-01-11 15:09:15 -0500

    test percel

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 import './../scss/main.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+render(<div>this is a test</div>, document.getElementById('root'));
